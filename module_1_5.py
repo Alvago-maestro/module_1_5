0a280e62d883fe6075a95d43efccf81331b2bbf4
Alvago-maestro <alvago-maestro@yandex.ru>
2024-08-29 20:45:31 +0600

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,7 @@
+immutable_var = 1, 5, 'sun', False
+print(immutable_var)
+#immutable_var[0]='car'
+#нельзя изменить элемент, потому что кортеж это неизменяемый список
+mutable_list = [8, 'dobro', 15, True]
+mutable_list[2]='room'
+print(mutable_list)
